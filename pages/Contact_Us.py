b01c5a59555bcfdb06bf6f598cf7b6c779dba89b
kiran <kiranjojan100@gmail.com>
2023-12-13 12:26:05 +0530

    Initial commit

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,25 +1,28 @@
 import streamlit as st
-
 from send_email import send_email
+import pandas
+
+
+df = pandas.read_csv("topics.csv")
+
 st.set_page_config(layout="wide")
 
 st.header("Contact US")
 
 with st.form(key="email form"):
     user_email = st.text_input("Your Email Address")
-    select_box = st.selectbox('What topic do you want to discuss?',
-                              ('Job Inquiries','Project Proposal','Other'))
+    option = st.selectbox(
+        'What topic do you want to discuss?',
+         df['topic'])
     raw_message = st.text_area("Text")
     message = f"""\
 Subject:New Email From {user_email} 
 
 From: {user_email}
-Topic: {select_box}
+Topic: {option}
 {raw_message}
 """
-    button = st.form_submit_button()
-    print(button)
-
+    button = st.form_submit_button("Submit")
     if button:
         send_email(message)
         st.info("your mail was send successfully")
